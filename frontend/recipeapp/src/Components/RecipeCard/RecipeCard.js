898e3df900c88cae05fe33289cb611a6aac268f8
alexaverill <alex.averill2013@gmail.com>
2023-12-17 15:23:00 -0600

    Style improvements

--- frontend/recipeapp/src/Components/RecipeCard/RecipeCard.js
+++ frontend/recipeapp/src/Components/RecipeCard/RecipeCard.js
@@ -5,7 +5,7 @@ import FavoriteButton from "../FavoriteButton/FavoriteButton";
 export default function RecipeCard({ recipe,favorited }) {
     const navigate = useNavigate();
     const collectionPills = recipe.collections?.map(collection => {
-        return <Chip label={collection.name} key={collection.collectionId}/>
+        return <Chip className={classes.chip} label={collection.name} key={collection.collectionId} href={`/collections/${collection.collectionId}`}/>
     })
     return (
         <div className={classes.card}>
@@ -14,16 +14,10 @@ export default function RecipeCard({ recipe,favorited }) {
 
                 </div>
                 <div className={classes.cardContent}>
-                    <div>
+                    <div className={classes.textContent}>
                         <div className={classes.cardTitle}>{recipe.title}</div>
                         <div className={classes.subtitle}>
                             <div>Contributed by {recipe.user?.username}</div>
-                            <div className={classes.seperator}>
-                                <svg width="5" height="6" viewBox="0 0 5 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <circle cx="2.5" cy="3" r="2.5" fill="#3CCEE2" />
-                                </svg>
-                            </div>
-                            <div>Variations</div>
                         </div>
                         <div className={classes.cardDesc}>
                             {recipe.description}
