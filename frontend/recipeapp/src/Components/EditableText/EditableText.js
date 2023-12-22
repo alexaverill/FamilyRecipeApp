c6b02a32f54713b2fe3232a7e19ea4fabe6246cc
alexaverill <alex.averill2013@gmail.com>
2023-12-22 09:44:19 -0600

    Add max length for titles

--- frontend/recipeapp/src/Components/EditableText/EditableText.js
+++ frontend/recipeapp/src/Components/EditableText/EditableText.js
@@ -6,7 +6,7 @@ import { Button } from '@mui/material';
 export default function EditableText({initialText,text,onChange}){
     const [isEditing,setEditing] = useState(false);
     if(isEditing){
-        return <TextField value={text} onBlur={()=>setEditing(false) }  onChange={onChange} size='small' autoFocus></TextField>
+        return <TextField value={text} onBlur={()=>setEditing(false) } inputProps={{ maxLength: 80 }} onChange={onChange} size='small' autoFocus></TextField>
     }
     return <div onClick={()=>setEditing(true)} className={text.length<=0 ? classes.placeholder : ''}><div className={classes.editRow}>{text.length>0?text:initialText}<Button><EditIcon/></Button></div></div>
 
