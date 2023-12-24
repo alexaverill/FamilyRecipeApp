94d17f4fca16d4bb406290896a95c18f0e17f6a3
alexaverill <alex.averill2013@gmail.com>
2023-12-24 07:32:01 -0600

    removing child from parent when deleted

--- backend/lambdas/src/deleteRecipe/index.js
+++ backend/lambdas/src/deleteRecipe/index.js
@@ -5,6 +5,18 @@ const docClient = DynamoDBDocumentClient.from(client);
 export const handler = async (event, context) => {
     console.log(event);
     try {
+        const getRecipe = new GetCommand({
+            TableName: "Recipes",
+            Key:{
+                recipeId:`${event.pathParameters.recipeId.toString()}`
+            }
+        });
+
+        const recipeResponse = await docClient.send(getRecipe);
+        let recipe = recipeResponse.Item;
+        if(recipe.parentId){
+            await removeRecipeIdFromParent(recipe.parentId,recipe.recipeId);
+        }
         const command = new DeleteCommand({
             TableName: "Recipes",
             Key:{
@@ -14,6 +26,7 @@ export const handler = async (event, context) => {
 
         const response = await docClient.send(command);
         let recipes = response.Item;
+        console.log(recipes);
         return {
             statusCode: 200,
             headers: {
@@ -32,5 +45,28 @@ export const handler = async (event, context) => {
     }
 
 };
+removeRecipeIdFromParent = async (parentId,recipeId) =>{
+    const getParent = new GetCommand({
+        TableName: "Recipes",
+        Key:{
+            recipeId:`${parentId}`
+        }
+    });
 
+    const parentRecipe = await docClient.send(getParent);
+    let recipe = parentRecipe.Item;
+    let childIndex = recipe.children?.findIndex(child => child === recipeId)
+
+    if (childIndex >= 0) {
+      recipe.children.splice(childIndex, 1);
+      console.log(recipe);
+      const command = new PutCommand({
+        TableName: "Recipes",
+        Item: recipe
+      });
+  
+      const response = await docClient.send(command);
+      console.log(response);
+    }
+}
 
