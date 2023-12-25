3ea68c27c65968fdaa1f0277963177bad241d1d6
alexaverill <alex.averill2013@gmail.com>
2023-12-25 09:58:00 -0600

    Adding ability to filter recipes by title or description

--- frontend/recipeapp/src/Components/Recipes/Recipes.js
+++ frontend/recipeapp/src/Components/Recipes/Recipes.js
@@ -6,9 +6,10 @@ import RecipeCard from "../RecipeCard/RecipeCard";
 import { UserContext } from "../UserContext/UserContext";
 import { GetRecipes } from "../../API/RecipeApi";
 export default function Recipes() {
-    const {user,favorites} = useContext(UserContext)
+    const { user, favorites } = useContext(UserContext)
     const [recipes, setRecipes] = useState([]);
-    const [isLoading,setIsLoading] = useState(false);
+    const [filteredRecipes, setFiltered] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
     useEffect(() => {
         LoadRecipes();
     }, [])
@@ -17,17 +18,27 @@ export default function Recipes() {
         let data = await GetRecipes();
         if (data) {
             setRecipes(data);
+            setFiltered(data);
         }
         setIsLoading(false);
     }
-    let recipeDisplay = recipes?.map((recipe) => {
-        if(favorites?.length>0 && favorites?.find((fav)=> { 
+    let recipeDisplay = filteredRecipes?.map((recipe) => {
+        if (favorites?.length > 0 && favorites?.find((fav) => {
             return fav === recipe.recipeId
-        })){
-            return <RecipeCard recipe={recipe} key={recipe.recipeId} favorited={true}/>; 
+        })) {
+            return <RecipeCard recipe={recipe} key={recipe.recipeId} favorited={true} />;
         }
         return <RecipeCard recipe={recipe} key={recipe.recipeId} favorited={false} />; //<Link to={'/recipe/' + recipe.recipeId} state={{ recipe }}>
     })
+    const filterRecipes = (e) => {
+        if (e.length > 1) {
+            let filtered = recipes.filter(recipe => recipe.title.includes(e) || recipe.description.includes(e));
+            setFiltered(filtered);
+        }
+        if (e.length == 0) {
+            setFiltered(recipes);
+        }
+    }
     return (
         <div className="content">
             <div className="recipeHeader">
@@ -35,12 +46,12 @@ export default function Recipes() {
                     <h1>Our Cookbook</h1>
                 </div>
                 <div className="search-and-add">
-                    {/* <TextField size="small" label="Search"></TextField> */}
+                    <TextField size="small" label="Search" onChange={(e) => filterRecipes(e.target.value)}></TextField>
                     <Link component="button" to="/create" className="recipeLinkButton">Add Recipe</Link>
                 </div>
             </div>
             <div className="recipeGrid">
-               {isLoading?<CircularProgress />: recipeDisplay}
+                {isLoading ? <CircularProgress /> : recipeDisplay}
             </div>
         </div>
     )
