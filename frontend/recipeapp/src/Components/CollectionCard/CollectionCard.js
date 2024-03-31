f125d5fafd1661bde0e79e6c9fb2ae8f211bd040
alexaverill <alex.averill2013@gmail.com>
2024-03-31 10:32:49 -0500

    Adding in a search field to new homepage

--- frontend/recipeapp/src/Components/CollectionCard/CollectionCard.js
+++ frontend/recipeapp/src/Components/CollectionCard/CollectionCard.js
@@ -4,7 +4,7 @@ export default function CollectionCard({ collection }) {
     const navigate = useNavigate();
     return (
         <div className={classes.card} onClick={()=>navigate('/collections/'+collection.collectionId,{state:{collection}})}>
-            <div classname={classes.content}>
+            <div className={classes.content}>
                 <div className={classes.title}>{collection.name}</div>
                 <div className={classes.subtitle}>{collection.recipes?.length} recipes</div>
             </div>
